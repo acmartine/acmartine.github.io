a7ca362f2b849d7b62bcf4787abf89c67ad504b2
acmartine <acmartine@gmail.com>
2016-03-16 16:49:25 -0400

    date.js update

--- cheese/js/date.js
+++ cheese/js/date.js
@@ -1,31 +1,22 @@
-//Try switch statements instead of if else
-function myFunction() {
-	var date = new Date(); //creates a Date() object
-	var weekday = date.getDay(); //getDay method for the Date() object. Tells you the index and assigns it to n
-		if (weekday == 1) {
-			document.getElementById('locale').innerHTML = "City Hall Plaza, Fisher Park";
-	} //options of index of n
-		else if (weekday == 2) {
-			document.getElementById('locale').innerHTML = "South End - Harrison Ave and East Concord St, by BMC";
-	}
-		else if (weekday == 3) {
-			document.getElementById('locale').innerHTML = "Back Bay - Trinity Place";
-	}
-		else if (weekday == 4) {
-			document.getElementById('locale').innerHTML = "South End - Harrison Ave and East Concord St, by BMC";
-	}
-		else if (weekday == 5) {
-			document.getElementById('locale').innerHTML = "Back Bay - Trinity Place";
-	}
-		else if (weekday == 6) {
-			document.getElementById('locale').innerHTML = "Dudley Square at Dudley Street";
-	}
-		else if (weekday == 0) {
-			document.getElementById('locale').innerHTML = "Closed on Sundays";
-	}
-		else {
-			document.getElementById('locale').innerHTML = "What dimension are you even in?";
-		}
+var date = new Date();
+var weekday = date.getDay();
+
+switch(weekday) {
+	case 1:
+		document.getElementById('locale').innerHTML = "City Hall Plaza, Fisher Park";
+		break;
+	case 2,4:
+		document.getElementById('locale').innerHTML = "South End - Harrison Ave and East Concord St, by BMC";
+		break;
+	case 3,5:
+		document.getElementById('locale').innerHTML = "Back Bay - Trinity Place";
+		break;
+	case 6:
+		document.getElementById('locale').innerHTML = "Dudley Square at Dudley Street";
+		break;
+	case 0:
+		document.getElementById('locale').innerHTML = "Closed on Sundays";
+		break;
+	default:
+		document.getElementById('locale').innerHTML = "Visit our Twitter for today's location!";
 }
-//CALL THE DAMN FUNCTION AFTER YOU WRITE IT!!!
-myFunction();
