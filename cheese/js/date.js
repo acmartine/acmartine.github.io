136b1421ffe60b78ae4a0dec8aafe0ec1e729d31
acmartine <acmartine@gmail.com>
2016-11-08 11:26:39 -0500

    cs script update

--- cheese/js/date.js
+++ cheese/js/date.js
@@ -1,22 +1,29 @@
-var date = new Date();
-var weekday = date.getDay();
-
-switch(weekday) {
-	case 1:
-		document.getElementById('locale').innerHTML = "City Hall Plaza, Fisher Park";
-		break;
-	case 2,4:
-		document.getElementById('locale').innerHTML = "South End - Harrison Ave and East Concord St, by BMC";
-		break;
-	case 3,5:
-		document.getElementById('locale').innerHTML = "Back Bay - Trinity Place";
-		break;
-	case 6:
-		document.getElementById('locale').innerHTML = "Dudley Square at Dudley Street";
-		break;
-	case 0:
-		document.getElementById('locale').innerHTML = "Closed on Sundays";
-		break;
-	default:
-		document.getElementById('locale').innerHTML = "Visit our Twitter for today's location!";
+function myFunction() {
+	var date = new Date(); 
+	var weekday = date.getDay(); 
+		if (weekday == 1) {
+			document.getElementById('locale').innerHTML = "City Hall Plaza, Fisher Park";
+	}
+		else if (weekday == 2) {
+			document.getElementById('locale').innerHTML = "South End - Harrison Ave and East Concord St, by BMC";
+	}
+		else if (weekday == 3) {
+			document.getElementById('locale').innerHTML = "Back Bay - Trinity Place";
+	}
+		else if (weekday == 4) {
+			document.getElementById('locale').innerHTML = "South End - Harrison Ave and East Concord St, by BMC";
+	}
+		else if (weekday == 5) {
+			document.getElementById('locale').innerHTML = "Back Bay - Trinity Place";
+	}
+		else if (weekday == 6) {
+			document.getElementById('locale').innerHTML = "Dudley Square at Dudley Street";
+	}
+		else if (weekday == 0) {
+			document.getElementById('locale').innerHTML = "Closed on Sundays";
+	}
+		else {
+			document.getElementById('locale').innerHTML = "Visit our Twitter for today's location!";
+		}
 }
+myFunction();
